0674ecf987fabe3743422013c60c31b01d3fda41
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 15:33:02 -0600

    feat - (JobService.Application) -  Add GetAllJobStatusQuery class just to keep the standar of CQRS

---
+++ src/Server/JobServer.Application/Queries/GetAllJobs/GetAllJobsQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace JobServer.Application.Queries.GetAllJobs
+{
+    /// <summary>
+    /// Represents the query to retrieve all jobs.
+    /// </summary>
+    public class GetAllJobsQuery
+    {
+    }
+}
