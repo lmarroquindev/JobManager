8cebc367e2c3b40d48c61cf96bd88a8300d17e6c
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 15:57:45 -0600

    feat - (JobService.Application) -  Add extension for dependency injection of Application services

---
+++ src/Server/JobServer.Application/DependencyInjection/ApplicationServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using JobServer.Application.Commands.CancelJob;
+using JobServer.Application.Commands.StartJob;
+using JobServer.Application.Queries.GetAllJobs;
+using JobServer.Application.Queries.GetJobStatus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JobServer.Application.DependencyInjection
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            // Command Handlers
+            services.AddTransient<IStartJobCommandHandler, StartJobCommandHandler>();
+            services.AddTransient<ICancelJobCommandHandler, CancelJobCommandHandler>();
+
+            // Query Handlers
+            services.AddTransient<IGetAllJobsQueryHandler, GetAllJobsQueryHandler>();
+            services.AddTransient<IGetJobStatusQueryHandler, GetJobStatusQueryHandler>();
+
+            return services;
+        }
+    }
+}
