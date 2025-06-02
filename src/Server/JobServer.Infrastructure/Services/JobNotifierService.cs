c4acdaed428e13aa618de2a5a5a3681a6628f073
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 15:54:48 -0600

    feat - (JobService.Application) -  Add JobNotifierService class to implement IJobNotifierService interface

---
+++ src/Server/JobServer.Infrastructure/Services/JobNotifierService.cs
@@ -0,0 +1,27 @@
+﻿using JobServer.Application.DTOs;
+using JobServer.Application.Interfaces;
+using JobServer.Application.Interfaces.Services;
+
+namespace JobServer.Infrastructure.Services
+{
+    /// <inheritdoc />
+    public class JobNotifierService : IJobNotifierService
+    {
+        private readonly IWebSocketService _webSocketService;
+
+        public JobNotifierService(IWebSocketService webSocketService)
+        {
+            _webSocketService = webSocketService;
+        }
+
+        /// <inheritdoc />
+        public async Task NotifyAsync(JobNotificationDto notification)
+        {
+            await _webSocketService.BroadcastMessageAsync(new WebSocketMessageDto(
+                notification.EventType,
+                notification.JobId,
+                notification.JobType
+            ));
+        }
+    }
+}
