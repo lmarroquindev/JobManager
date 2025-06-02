ba5f05fa3fa7f4637648496c62efa80244e130c8
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 15:00:40 -0600

    feat - (JobService.Application) -  Add GetJobStatusQuery class to contain the parameters to get the status of a specific job

---
+++ src/Server/JobServer.Application/Queries/GetJobStatus/GetJobStatusQuery.cs
@@ -0,0 +1,17 @@
+﻿namespace JobServer.Application.Queries.GetJobStatus
+{
+    /// <summary>
+    /// Represents the query to retrieve the status of a specific job.
+    /// </summary>
+    public class GetJobStatusQuery
+    {
+        public GetJobStatusQuery(Guid jobId)
+        {
+            JobId = jobId;
+        }
+        /// <summary>
+        /// Gets or sets the unique identifier of the job.
+        /// </summary>
+        public Guid JobId { get; set; }
+    }
+}
