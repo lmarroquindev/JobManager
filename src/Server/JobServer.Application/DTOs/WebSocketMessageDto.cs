0989c900c7a2f339cab4dbc9c85c5a42a9f3491e
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 15:37:37 -0600

    feat - (JobService.Application) -  Add WebSocketMessageDto class to contain the parameter for the websocket created

---
+++ src/Server/JobServer.Application/DTOs/WebSocketMessageDto.cs
@@ -0,0 +1,30 @@
+﻿namespace JobServer.Application.DTOs
+{
+    /// <summary>
+    /// Represents a message to be broadcasted to WebSocket clients.
+    /// </summary>
+    public class WebSocketMessageDto
+    {
+        public WebSocketMessageDto(string eventType, Guid jobId, string jobType)
+        {
+            EventType = eventType;
+            JobId = jobId;
+            JobType = jobType;
+        }
+
+        /// <summary>
+        /// Gets or sets the type of the event (e.g., started, completed, canceled).
+        /// </summary>
+        public string EventType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unique identifier of the job.
+        /// </summary>
+        public Guid JobId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the job.
+        /// </summary>
+        public string JobType { get; set; }
+    }
+}
