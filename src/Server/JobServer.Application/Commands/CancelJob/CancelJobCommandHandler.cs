675c6b67182f070acf422b138ec736ab88652ba0
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 14:56:09 -0600

    feat - (JobService.Application) -  AddCancelJobCommandHandler class to call cancelJobAsync function and cancel a live job

---
+++ src/Server/JobServer.Application/Commands/CancelJob/CancelJobCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using JobServer.Domain.Interfaces;
+
+namespace JobServer.Application.Commands.CancelJob
+{
+    /// <summary>
+    /// Defines the contract for handling the <see cref="CancelJobCommand"/>.
+    /// </summary>
+    public interface ICancelJobCommandHandler
+    {
+        /// <summary>
+        /// Handles the cancellation of a job.
+        /// </summary>
+        /// <param name="command">The command containing the job ID to cancel.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result indicates whether the cancellation was successful.</returns>
+        Task<bool> HandleAsync(CancelJobCommand command);
+    }
+
+    /// <inheritdoc />
+    public class CancelJobCommandHandler : ICancelJobCommandHandler
+    {
+        private readonly IJobExecutor _executor;
+
+        public CancelJobCommandHandler(IJobExecutor executor)
+        {
+            _executor = executor;
+        }
+
+        /// <inheritdoc />
+        public Task<bool> HandleAsync(CancelJobCommand command)
+        {
+            return _executor.CancelJobAsync(command.JobId);
+        }
+    }
+}
