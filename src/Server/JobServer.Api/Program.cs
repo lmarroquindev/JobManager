16c4cba70f222c4cde4c290dde7163f2d28f3eac
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 15:59:46 -0600

    feat - (JobService.Api) -  Add dependency injection configuration, error handler configuration and websocket configuration in to program class

--- src/Server/JobServer.Api/Program.cs
+++ src/Server/JobServer.Api/Program.cs
@@ -1,25 +1,67 @@
+﻿using JobServer.API.Middleware;
+using JobServer.Application.DependencyInjection;
+using JobServer.Application.Interfaces;
+using JobServer.Domain.Entities;
+using JobServer.Infrastructure;
+using System.Collections.Concurrent;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Shared in-memory job store
+var jobStore = new ConcurrentDictionary<Guid, Job>();
+builder.Services.AddSingleton(jobStore);
+
+// Infrastructure
+builder.Services.AddInfrastructure(jobStore);
+
+// Application
+builder.Services.AddApplicationServices();
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
+app.UseCors("AllowFrontend");
 app.UseAuthorization();
+app.UseWebSockets();
 
 app.MapControllers();
+
+// WebSocket endpoint
+app.Map("/ws", async context =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+        var webSocketService = context.RequestServices.GetRequiredService<IWebSocketService>();
+        await webSocketService.HandleClientAsync(webSocket);
+    }
+    else
+    {
+        context.Response.StatusCode = 400;
+    }
+});
 
 app.Run();
