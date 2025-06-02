91984de64cd1bc0d7e1fc5aab6812bdbf1bfe641
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 14:48:30 -0600

    feat - (JobService.Application) -  Add StartJobCommandHandler class to call startJobAsync function and create a new job

---
+++ src/Server/JobServer.Application/Commands/StartJob/StartJobCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using JobServer.Domain.Interfaces;
+
+namespace JobServer.Application.Commands.StartJob
+{
+    /// <summary>
+    /// Defines the contract for handling the <see cref="StartJobCommand"/>.
+    /// </summary>
+    public interface IStartJobCommandHandler
+    {
+        /// <summary>
+        /// Handles the execution of the <see cref="StartJobCommand"/>.
+        /// </summary>
+        /// <param name="command">The command containing job details.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the unique identifier of the started job.</returns>
+        Task<Guid> HandleAsync(StartJobCommand command);
+    }
+
+    /// <inheritdoc />
+    public class StartJobCommandHandler : IStartJobCommandHandler
+    {
+        private readonly IJobExecutor _jobExecutor;
+
+        public StartJobCommandHandler(IJobExecutor jobExecutor)
+        {
+            _jobExecutor = jobExecutor;
+        }
+
+        /// <inheritdoc />
+        public async Task<Guid> HandleAsync(StartJobCommand command)
+        {
+            return await _jobExecutor.StartJobAsync(command.JobType, command.JobName);
+        }
+    }
+}
