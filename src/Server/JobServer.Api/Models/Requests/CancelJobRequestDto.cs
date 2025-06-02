a5324b40421fe14dc219380e04ac77d17495e686
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 15:35:42 -0600

    feat - (JobService.Api) -  Add CancelJobRequestDto to receive the parameters from client to cancel a job

---
+++ src/Server/JobServer.Api/Models/Requests/CancelJobRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobServer.API.Models.Requests
+{
+    /// <summary>
+    /// Represents the input data required to cancel a job.
+    /// </summary>
+    public class CancelJobRequestDto
+    {
+        /// <summary>
+        /// The unique identifier of the job to cancel.
+        /// </summary>
+        [Required(ErrorMessage = "JobId is required.")]
+        public Guid JobId { get; set; }
+    }
+}
