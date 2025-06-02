105179040b57aeae694ca056e8c5de0b6503cad9
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 15:57:21 -0600

    feat - (JobService.Infraestructure) -  Add extension for dependency injection of Infraestructure services

---
+++ src/Server/JobServer.Infrastructure/DependencyInjection/InfrastructureServiceRegistration.cs
@@ -0,0 +1,30 @@
+﻿using JobServer.Application.Interfaces;
+using JobServer.Application.Interfaces.Services;
+using JobServer.Domain.Entities;
+using JobServer.Domain.Interfaces;
+using JobServer.Infrastructure.Persistence;
+using JobServer.Infrastructure.Services;
+using JobServer.Infrastructure.WebSockets;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Concurrent;
+
+namespace JobServer.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConcurrentDictionary<Guid, Job> jobStore)
+        {
+            services.AddSingleton<IJobExecutor>(sp =>
+            {
+                var notifier = sp.GetRequiredService<IJobNotifierService>();
+                return new InMemoryJobExecutorAdapter(jobStore, notifier);
+            });
+
+            services.AddSingleton<IJobQueryService>(sp => new InMemoryJobQueryAdapter(jobStore));
+            services.AddSingleton<IJobNotifierService, JobNotifierService>();
+            services.AddSingleton<IWebSocketService, WebSocketService>();
+
+            return services;
+        }
+    }
+}
