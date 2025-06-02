67c7fb8e7b6b4599f832377a34629aaf444a3617
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 13:55:07 -0600

    feat - (JobService.Infraestructure) -  Add implementation class for IJobQueryService interface to implement methods for get all jobs or state of a single job

---
+++ src/Server/JobServer.Infrastructure/Persistence/InMemoryJobQueryAdapter.cs
@@ -0,0 +1,34 @@
+﻿using JobServer.Domain.Entities;
+using JobServer.Domain.Interfaces;
+using System.Collections.Concurrent;
+
+namespace JobServer.Infrastructure.Persistence
+{
+    /// <inheritdoc />
+    public class InMemoryJobQueryAdapter : IJobQueryService
+    {
+        private readonly ConcurrentDictionary<Guid, Job> _jobs;
+
+        public InMemoryJobQueryAdapter(ConcurrentDictionary<Guid, Job> jobs)
+        {
+            _jobs = jobs;
+        }
+
+        /// <inheritdoc />
+        public Task<IEnumerable<Job>> GetAllJobsAsync()
+        {
+            return Task.FromResult(_jobs.Values.AsEnumerable());
+        }
+
+        /// <inheritdoc />
+        public Task<string> GetJobStatusAsync(Guid jobId)
+        {
+            if (_jobs.TryGetValue(jobId, out var job))
+            {
+                return Task.FromResult(job.IsRunning ? "Running" : "Not Running");
+            }
+
+            return Task.FromResult("Not Found");
+        }
+    }
+}
