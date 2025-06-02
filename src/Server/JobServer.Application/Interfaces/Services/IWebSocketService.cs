3901530c13506062499de355b89e5276bb1c668c
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 15:40:36 -0600

    feat - (JobService.Application) -  Add IWebSocketService interface with the definition of methods for BroadcastMessage and initialize websocket

---
+++ src/Server/JobServer.Application/Interfaces/Services/IWebSocketService.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.WebSockets;
+using JobServer.Application.DTOs;
+
+namespace JobServer.Application.Interfaces
+{
+    /// <summary>
+    /// Defines a contract for managing WebSocket connections and broadcasting messages.
+    /// </summary>
+    public interface IWebSocketService
+    {
+        /// <summary>
+        /// Handles a new WebSocket client connection.
+        /// </summary>
+        /// <param name="socket">The WebSocket client.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task HandleClientAsync(WebSocket socket);
+
+        /// <summary>
+        /// Broadcasts a message to all connected WebSocket clients.
+        /// </summary>
+        /// <param name="message">The message to broadcast.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task BroadcastMessageAsync(WebSocketMessageDto message);
+    }
+}
