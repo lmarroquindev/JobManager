b525cbdd474649cbd64cd5e65b2c179541a2365f
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 13:57:43 -0600

    feat - (JobService.Application) -  Add StartJobCommand class to contain input parameters to start a new job

---
+++ src/Server/JobServer.Application/Commands/StartJob/StartJobCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace JobServer.Application.Commands.StartJob
+{
+    /// <summary>
+    /// Represents the command to start a new asynchronous job.
+    /// </summary>
+    public class StartJobCommand
+    {
+        public StartJobCommand(string jobType, string jobName)
+        {
+            JobType = jobType;
+            JobName = jobName;
+        }
+
+        /// <summary>
+        /// Gets or sets the type of the job to be executed.
+        /// </summary>
+        public string JobType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the job to be executed.
+        /// </summary>
+        public string JobName { get; set; }
+    }
+}
