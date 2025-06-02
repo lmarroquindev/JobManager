d644f650263a699f7521657a78d96daaf8c52a8b
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 15:33:18 -0600

    feat - (JobService.Application) -  Add GetJobStatusQueryHandler class to call the GetJobStatusAsync method to get all jobs data

---
+++ src/Server/JobServer.Application/Queries/GetAllJobs/GetAllJobsQueryHandler.cs
@@ -0,0 +1,43 @@
+﻿using JobServer.Application.DTOs;
+using JobServer.Domain.Interfaces;
+
+namespace JobServer.Application.Queries.GetAllJobs
+{
+    /// <summary>
+    /// Defines the contract for handling the <see cref="GetAllJobsQuery"/>.
+    /// </summary>
+    public interface IGetAllJobsQueryHandler
+    {
+        /// <summary>
+        /// Handles the query to retrieve all jobs.
+        /// </summary>
+        /// <param name="query">The query object.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a collection of job DTOs.</returns>
+        Task<IEnumerable<JobDto>> HandleAsync(GetAllJobsQuery query);
+    }
+
+    /// <inheritdoc />
+    public class GetAllJobsQueryHandler : IGetAllJobsQueryHandler
+    {
+        private readonly IJobQueryService _jobQueryService;
+
+        public GetAllJobsQueryHandler(IJobQueryService jobQueryService)
+        {
+            _jobQueryService = jobQueryService;
+        }
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<JobDto>> HandleAsync(GetAllJobsQuery query)
+        {
+            var jobs = await _jobQueryService.GetAllJobsAsync();
+
+            return jobs.Select(job => new JobDto
+            {
+                Id = job.Id,
+                JobType = job.JobType,
+                JobName = job.JobName,
+                Status = job.IsRunning ? "Running" : "Not Running"
+            });
+        }
+    }
+}
