527c14cd4b17f8c3d463642da744620ecb979a12
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 14:54:10 -0600

    feat - (JobService.Application) -  Add CancelJobCommand class to contain input parameters to cancel a job

---
+++ src/Server/JobServer.Application/Commands/CancelJob/CancelJobCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace JobServer.Application.Commands.CancelJob
+{
+    /// <summary>
+    /// Represents the command to cancel a running job.
+    /// </summary>
+    public class CancelJobCommand
+    {
+        public CancelJobCommand(Guid jobId)
+        {
+            JobId = jobId;
+        }
+
+        /// <summary>
+        /// Gets or sets the unique identifier of the job to cancel.
+        /// </summary>
+        public Guid JobId { get; set; }
+    }
+}
