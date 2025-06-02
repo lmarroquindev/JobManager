a0b63a35b86cb99de89d4cb1617508436f72d50b
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 15:05:36 -0600

    feat - (JobService.Application) -  Add GetJobStatusQueryHandler class to call the GetJobStatusAsync method to get all jobs data

---
+++ src/Server/JobServer.Application/Queries/GetJobStatus/GetJobStatusQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using JobServer.Domain.Interfaces;
+
+namespace JobServer.Application.Queries.GetJobStatus
+{
+    /// <summary>
+    /// Defines the contract for handling the <see cref="GetJobStatusQuery"/>.
+    /// </summary>
+    public interface IGetJobStatusQueryHandler
+    {
+        /// <summary>
+        /// Handles the query to retrieve the status of a specific job.
+        /// </summary>
+        /// <param name="query">The query containing the job ID.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the job status as a string.</returns>
+        Task<string> HandleAsync(GetJobStatusQuery query);
+    }
+
+    /// <inheritdoc />
+    public class GetJobStatusQueryHandler : IGetJobStatusQueryHandler
+    {
+        private readonly IJobQueryService _queryService;
+
+        public GetJobStatusQueryHandler(IJobQueryService queryService)
+        {
+            _queryService = queryService;
+        }
+
+        /// <inheritdoc />
+        public Task<string> HandleAsync(GetJobStatusQuery query)
+        {
+            return _queryService.GetJobStatusAsync(query.JobId);
+        }
+    }
+}
