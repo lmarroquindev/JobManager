4084f0efa8f0d3d666d9a54a19af699dc8212ef7
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 13:50:20 -0600

    feat - (JobService.Domain) -  Add business interface for the adapter with methods to read all jobs or jobs status

---
+++ src/Server/JobServer.Domain/Interfaces/IJobQueryService.cs
@@ -0,0 +1,23 @@
+﻿using JobServer.Domain.Entities;
+
+namespace JobServer.Domain.Interfaces
+{
+    /// <summary>
+    /// Provides methods to query job information from the system.
+    /// </summary>
+    public interface IJobQueryService
+    {
+        /// <summary>
+        /// Retrieves all jobs currently tracked by the system.
+        /// </summary>
+        /// <returns>A collection of all jobs.</returns>
+        Task<IEnumerable<Job>> GetAllJobsAsync();
+
+        /// <summary>
+        /// Retrieves the current status of a specific job.
+        /// </summary>
+        /// <param name="jobId">The unique identifier of the job.</param>
+        /// <returns>A string representing the job status.</returns>
+        Task<string> GetJobStatusAsync(Guid jobId);
+    }
+}
