0e8045c23483895151b16778d10f45a6845618ad
lmarroquindev <luismarroquindev@gmail.com>
2025-06-02 15:54:24 -0600

    feat - (JobService.Application) -  Add IJobNotifierService interface with the definition of a method to create the notification in the websocket

---
+++ src/Server/JobServer.Application/Interfaces/Services/IJobNotifierService.cs
@@ -0,0 +1,17 @@
+﻿using JobServer.Application.DTOs;
+
+namespace JobServer.Application.Interfaces.Services
+{
+    /// <summary>
+    /// Defines a contract for notifying clients about job-related events.
+    /// </summary>
+    public interface IJobNotifierService
+    {
+        /// <summary>
+        /// Sends a notification about a job event to connected clients.
+        /// </summary>
+        /// <param name="notification">The job notification data.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task NotifyAsync(JobNotificationDto notification);
+    }
+}
