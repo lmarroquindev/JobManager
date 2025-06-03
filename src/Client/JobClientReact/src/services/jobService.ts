d0b0611893c21d0266f534b9217fdc9645ed0e01
lmarroquindev <luismarroquindev@gmail.com>
2025-06-03 01:43:35 -0600

    fix - (JobClient.React) - Implement call to api axios interceptor in every request from service

--- src/Client/JobClientReact/src/services/jobService.ts
+++ src/Client/JobClientReact/src/services/jobService.ts
@@ -1,24 +1,22 @@
-import axios from 'axios';
+import api from './apiClient';
 import type { Job } from '../types';
 
-const API_BASE = 'http://localhost:5292/api/Jobs';
-
 export const startJob = async (jobType: string, jobName: string) => {
-  const response = await axios.post(`${API_BASE}`, { jobType, jobName });
+  const response = await api.post('/Jobs', { jobType, jobName });
   return response.data;
 };
 
 export const getJobs = async (): Promise<Job[]> => {
-  const response = await axios.get(`${API_BASE}`);
+  const response = await api.get('/Jobs');
   return response.data;
 };
 
 export const getJobStatus = async (id: string): Promise<string> => {
-  const response = await axios.get(`${API_BASE}/job-status/${id}`);
+  const response = await api.get(`/Jobs/job-status/${id}`);
   return response.data.status;
 };
 
-export const cancelJob = async (id: string) => {
-  const response = await axios.post(`${API_BASE}/cancel-job`, { id });
+export const cancelJob = async (jobId: string) => {
+  const response = await api.post('/Jobs/cancel-job', { jobId });
   return response.data;
 };
